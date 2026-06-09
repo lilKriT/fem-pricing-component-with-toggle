b398b49f67389de232a6a4d78fb2c3d0939458b8
lilKriT <lilKriTwebDev@gmail.com>
2026-06-10 00:01:04 +0200

    Removed files. Added favicon. Added font. Added basic styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="">Hi.</div>;
+  return (
+    <div className="">
+      <p>Our Pricing Annually Monthly</p>
+
+      <p>
+        Basic &dollar;19.99 &dollar;199.99 500 GB Storage 2 Users Allowed Send
+        up to 3 GB Learn More
+      </p>
+
+      <p>
+        Professional &dollar;24.99 &dollar;249.99 1 TB Storage 5 Users Allowed
+        Send up to 10 GB Learn More
+      </p>
+
+      <p>
+        Master &dollar;39.99 &dollar;399.99 2 TB Storage 10 Users Allowed Send
+        up to 20 GB Learn More
+      </p>
+    </div>
+  );
 }
